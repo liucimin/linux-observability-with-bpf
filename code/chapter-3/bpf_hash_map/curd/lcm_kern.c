1c81018177c1d3bfff5328f5c23cd806d5deb33d
Jackson <lcmjkson@gmail.com>
2021-05-10 19:45:41 +0800

    add bpf_trace_printk log position

--- code/chapter-3/bpf_hash_map/curd/lcm_kern.c
+++ code/chapter-3/bpf_hash_map/curd/lcm_kern.c
@@ -41,7 +41,22 @@ int bpf_prog(void *ctx) {
 int value = 1024;
   bpf_map_update_elem(&hash_map, &key, &value, BPF_ANY);
   char msg[] = "Hello, BPF World!";
-  bpf_trace_printk(msg, sizeof(msg));
+  bpf_trace_printk(msg, sizeof(msg));   //参考 https://blog.csdn.net/notbaron/article/details/80256549
+  /*
+  4.1     bpf_trace_printk
+语法：int bpf_trace_printk(const char *fmt, int fmt_size, ...)
+
+简单的内核printf函数，输出到/sys/kernel/debug/tracing/trace_pipe。这个在之前有描述过，其最多３个参数，智能输出%s，
+而且trace_pipe是全局共享的并发输出会有问题，生产中工具使用BPF_PERF_OUTPUT()函数。
+
+4.2     BPF_PERF_OUTPUT
+通过perf ring buffer创建BPF表，将定义的事件数据输出。这个是将数据推送到用户态的建议方法。
+
+4.3     perf_submit
+语法：int perf_submit((void *)ctx, (void *)data, u32 data_size)
+
+该函数是BPF_PERF_OUTPUT表的方法，将定义的事件数据推到用户态。
+  */
   return 0;
 }
 
